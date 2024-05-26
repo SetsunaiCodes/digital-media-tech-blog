f140928bfb2ebf380227db280fb48a29f3c78a9e
SetsunaiCodes <nicojin.m0sic@hotmail.com>
2024-05-26 14:10:36 +0200

    added ShadCN

--- dmt/src/app/page.tsx
+++ dmt/src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 
 export default function Home() {
   return (
     <div>
-      
+      <h1>Test Text</h1>
+      <Button>Click Me!</Button>
     </div>
   )
 }

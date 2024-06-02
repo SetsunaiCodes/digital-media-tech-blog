f489c41fae6ca0745021a27debf58da3180bc55e
SetsunaiCodes <nicojin.m0sic@hotmail.com>
2024-06-02 14:00:10 +0200

    added interface

--- dmt/src/app/page.tsx
+++ dmt/src/app/page.tsx
@@ -1,87 +1,70 @@
-
-//NextJS Imports
-import { Link } from "lucide-react";
-import Image from "next/image";
-
-//Contentlayer Imports
+// Contentlayer Imports
 import { allDocs, Doc } from "contentlayer/generated";
 import { compareDesc, format, parseISO } from "date-fns";
 
-
-//ShadCN UI
+// ShadCN UI
 import {
   Card,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-
-
-
-//Base Layout for the Cards
-export function ArticleCard(post: Doc){
-  const path: string = "blog/" + post.slugParams;
-
-
-  return(
-  
-  <Card>
-    <a href={path}>
-      <img src={post.img} alt={post.title} />
-    </a>
-    <CardHeader>
-      <CardTitle>{post.title}</CardTitle>
-      <CardDescription>{post.des}</CardDescription>
-    </CardHeader>
-  </Card>
-  )
+// FIXME: Hier fehlt ein Interface für das Doc
+interface ArtikelInterface {
+  title: string;
+  des: string;
+  date: string;
+  slugParams: string;
+  img: string;
 }
 
-//Root Component
-export default function Home() {
-  const posts = allDocs.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
+// Base Layout for the Cards
+export function ArticleCard({ title, des, date, slugParams, img }: ArtikelInterface) {
+  const path: string = "blog/" + slugParams;
 
   return (
-        <div>
-          <main className="w-80p m-auto">
-            <h1 className="text-3xl font-bold">Geschriebene Artikel</h1>
+    <Card>
+      <a href={path}>
+        <img src={img} alt={title} />
+      </a>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{des}</CardDescription>
+      </CardHeader>
+    </Card>
+  );
+}
 
-            <Tabs defaultValue="devlogs" className="w-[100p]">
-            <TabsList className="grid w-[20vw] grid-cols-2">
-              <TabsTrigger value="devlogs">DevLogs</TabsTrigger>
-              <TabsTrigger value="veranstaltungen">Veranstaltungen</TabsTrigger>
-            </TabsList>
-            <TabsContent value="devlogs"> 
+// Root Component
+export default function Home() {
+  const posts = allDocs.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
 
-             <div className="flex flex-wrap gap-5 justify-center">
-              {/*Content of first tab*/}
+  return (
+    <div>
+      <main className="w-80p m-auto">
+        <h1 className="text-3xl font-bold">Geschriebene Artikel</h1>
+
+        <Tabs defaultValue="devlogs" className="w-[100p]">
+          <TabsList className="grid w-[20vw] grid-cols-2">
+            <TabsTrigger value="devlogs">DevLogs</TabsTrigger>
+            <TabsTrigger value="veranstaltungen">Veranstaltungen</TabsTrigger>
+          </TabsList>
+          <TabsContent value="devlogs">
+            <div className="flex flex-wrap gap-5 justify-center">
+              {/* Content of first tab */}
               {posts.map((post, idx) => (
-
-                  <ArticleCard key={idx} {...post} />
-
-              
-                ))}
-              </div>
-
-            </TabsContent>
-            <TabsContent value="veranstaltungen">
-              {/*Content of second tab*/}
-
-
-            </TabsContent>
-          </Tabs>
-
-
-
-
-
-
-
-
-          </main>
-      </div>
-  )
+                <ArticleCard key={idx} {...post} />
+              ))}
+            </div>
+          </TabsContent>
+          <TabsContent value="veranstaltungen">
+            {/* Content of second tab */}
+          </TabsContent>
+        </Tabs>
+      </main>
+    </div>
+  );
 }

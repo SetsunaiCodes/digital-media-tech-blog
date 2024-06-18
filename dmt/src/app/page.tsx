f9687722b88b12c7f9fc7d057969e49d3412a1aa
SetsunaiCodes <nicojin.m0sic@hotmail.com>
2024-06-18 09:02:04 +0200

    changed highlight and light and darkmode button

--- dmt/src/app/page.tsx
+++ dmt/src/app/page.tsx
@@ -26,7 +26,8 @@ import {
   CommandShortcut,
 } from "@/components/ui/command";
 
-import { ModeToggle } from "@/components/ui/themebutton";
+//Custom Components
+import Highlight from "@/components/custom/highlight";
 
 interface ArtikelInterface {
   title: string;
@@ -43,7 +44,7 @@ function ArticleCard({ title, des, date, slugParams, img }: ArtikelInterface) {
   return (
     <Card>
       <a href={path}>
-        <img src={img} alt={title} />
+        <img src={img} alt={title} className="object-cover rounded-sm rounded-b-none" />
       </a>
       <CardHeader>
         <CardTitle>{title}</CardTitle>
@@ -68,49 +69,53 @@ export default function Home() {
   );
 
   return (
+    <div>
+      <Highlight/>
     <div className="flex bg-white dark:bg-slate-900">
-      <main className="w-70p">
-        <h1 className="text-3xl font-bold">Geschriebene Artikel</h1>
-        <ModeToggle />
-        <Tabs defaultValue="devlogs" className="w-[100p]">
-          <TabsList className="grid w-[20vw] grid-cols-2">
-            <TabsTrigger value="devlogs">DevLogs</TabsTrigger>
-            <TabsTrigger value="veranstaltungen">Veranstaltungen</TabsTrigger>
-          </TabsList>
-          <TabsContent value="devlogs">
-            <div className="flex flex-wrap gap-5 justify-center">
-              {/* Content of first tab */}
-              {posts.map((post, idx) => (
-                <ArticleCard key={idx} {...post} />
-              ))}
-            </div>
-          </TabsContent>
-          <TabsContent value="veranstaltungen">
-            {/* Content of second tab */}
-          </TabsContent>
-        </Tabs>
-      </main>
+          <main className="w-70p">
+            <h1 className="text-3xl font-bold">Geschriebene Artikel</h1>
+            <Tabs defaultValue="devlogs" className="w-[100p]">
+              <TabsList className="grid w-[20vw] grid-cols-2">
+                <TabsTrigger value="devlogs">DevLogs</TabsTrigger>
+                <TabsTrigger value="veranstaltungen">Veranstaltungen</TabsTrigger>
+              </TabsList>
+              <TabsContent value="devlogs">
+                <div className="flex flex-wrap gap-5 justify-center">
+                  {/* Content of first tab */}
+                  {posts.map((post, idx) => (
+                    <ArticleCard key={idx} {...post} />
+                  ))}
+                </div>
+              </TabsContent>
+              <TabsContent value="veranstaltungen">
+                {/* Content of second tab */}
+              </TabsContent>
+            </Tabs>
+          </main>
 
-      <aside className="w-30p mt-20 pl-5 pr-5">
-        <Command>
-          <CommandInput 
-            placeholder="Type a command or search..." 
-            onValueChange={handleSearchChange}
-          />
-          <CommandList>
-            <CommandEmpty>No results found.</CommandEmpty>
-            <CommandGroup heading="Articles">
-              {filteredPosts.map((post, idx) => (
-                <CommandItem key={idx} onSelect={() => window.location.href = `blog/${post.slugParams}`}>
-                  {post.title}
-                  <CommandShortcut>{format(parseISO(post.date), "PPP")}</CommandShortcut>
-                </CommandItem>
-              ))}
-            </CommandGroup>
-            <CommandSeparator />
-          </CommandList>
-        </Command>
-      </aside>
+          <aside className="w-30p mt-20 pl-5 pr-5">
+            <Command>
+              <CommandInput 
+                placeholder="Type a command or search..." 
+                onValueChange={handleSearchChange}
+              />
+              <CommandList>
+                <CommandEmpty>No results found.</CommandEmpty>
+                <CommandGroup heading="Articles">
+                  {filteredPosts.map((post, idx) => (
+                    <CommandItem key={idx} onSelect={() => window.location.href = `blog/${post.slugParams}`}>
+                      {post.title}
+                      <CommandShortcut>{format(parseISO(post.date), "PPP")}</CommandShortcut>
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+                <CommandSeparator />
+              </CommandList>
+            </Command>
+          </aside>
+        </div>
+
+
     </div>
   );
 }

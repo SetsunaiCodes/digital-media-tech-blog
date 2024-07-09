da91ab62c4984abdfdeb3bdb8fdc078a70342cac
SetsunaiCodes <nicojin.m0sic@hotmail.com>
2024-07-09 03:02:28 +0200

    Corrected Articles

--- dmt/src/components/Mdx.tsx
+++ dmt/src/components/Mdx.tsx
@@ -4,21 +4,21 @@ import { useMDXComponent } from "next-contentlayer/hooks";
 const components = {
   h1: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
     <h1
-      className={"mt-5 scroll-m-20 text-4xl font-bold tracking-tight"}
+      className={"mt-10 mb-2 scroll-m-20 text-4xl font-bold tracking-tight"}
       {...props}
     />
   ),
 
   h2: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
     <h2
-      className={"mt-2 mb-1 scroll-m-20 text-3xl font-bold tracking-tight"}
+      className={"mt-8 mb-2 scroll-m-20 text-3xl font-bold tracking-tight"}
       {...props}
     />
   ),
 
   h3: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
     <h3
-      className={"mt-2 scroll-m-20 text-2xl font-bold tracking-tight"}
+      className={"mt-6 scroll-m-20 text-2xl font-bold tracking-tight"}
       {...props}
     />
   ),
@@ -30,6 +30,11 @@ const components = {
   em: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
     <em className={"  border-l-4 border-white mt-2 text-1xl rounded-sm"} {...props} />
   ),
+
+  hr: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <hr className={"mt-8"} {...props} />
+  )
+
 };
 
 interface MdxProps {
